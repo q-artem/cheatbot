3d450f53247eb1f648a72af06d9f7ceb37c86ac3
RofIeX <Stodvalista@yandex.ru>
2023-09-20 01:01:15 +0500

    add state users add a simple menu configs and select watch

--- functions.py
+++ functions.py
@@ -1,10 +1,13 @@
 import asyncio
+from aiogram import types
 
-from bot import inputs
-from utils import *
+import global_variables
+from configs import CHOOSING_WATCH_TEXT, IN_CHOICE_WATCH_STATE, IN_SLEEP_STATE, BACK_TEXT, SETTINGS_TEXT, \
+    IN_SETTINGS_STATE
+from utils import update_keyboard, get_value_from_id, write_value_from_id, cut_into_messages, debug
 
 
-async def service_block(message):
+async def service_block(message: types.Message):
     if message.text == "Здохни":
         await message.answer('Неа, всё, отладка больше не работает')
         return True
@@ -13,36 +16,68 @@ async def service_block(message):
         await message.answer('Okk')
         exit(0)
 
+    if message.text == CHOOSING_WATCH_TEXT:
+        global_variables.states[message.from_user.id] = IN_CHOICE_WATCH_STATE
+        await message.answer('Выберите свои часы из меню:', reply_markup=await update_keyboard(message))
+        return True
+
+    if message.text == SETTINGS_TEXT:
+        global_variables.states[message.from_user.id] = IN_SETTINGS_STATE
+        await message.answer('Выберите параметр из меню:', reply_markup=await update_keyboard(message))
+        return True
+
+    if message.text == BACK_TEXT and (global_variables.states[message.from_user.id] == IN_CHOICE_WATCH_STATE or
+                                      global_variables.states[message.from_user.id] == IN_SETTINGS_STATE):
+        global_variables.states[message.from_user.id] = IN_SLEEP_STATE
+        await message.answer('Готов к работе!', reply_markup=await update_keyboard(message))
+        return True
+
     if message.text == 'Моего браслета нет в списке':
         await message.answer(
-            'Ойй, не, ну а чего ты хотел, чтобы ещё и сторонние браслеты добавлять? '
+            'Ой, не, ну а чего ты хотел, чтобы ещё и сторонние браслеты добавлять? '
             'Мне за это не платят. Может потом сделаю')
         return True
     return False
 
 
-async def set_watch(message):
+async def set_watch(message: types.Message):
+    if global_variables.states[message.from_user.id] != IN_CHOICE_WATCH_STATE:
+        return False
     if not await get_value_from_id(message.text, table="watches", fields="id", sign_column="name") is None:
         await write_value_from_id(
             message.from_user.id, "selectedWatch", await get_value_from_id(
                 message.text, table="watches", fields="id", sign_column="name"))
-        await message.answer('Принято')
+        global_variables.states[message.from_user.id] = IN_SLEEP_STATE
+        await message.answer('Принято.\nГотов к работе!', reply_markup=await update_keyboard(message))
         return True
+    await message.answer('Сначала выберите часы из меню или вернитесь назад')
     return False
 
 
-async def send_cheats(message):
+async def set_settings(message: types.Message):
+    if global_variables.states[message.from_user.id] != IN_SETTINGS_STATE:
+        return False
+
+    pass
+
+    await message.answer('Сорян, пока без настроек')
+    return False
+
+
+async def send_cheats(message: types.Message):
+    if global_variables.states[message.from_user.id] != IN_SLEEP_STATE:
+        return True
     first_call = True
-    if message.from_user.id in inputs.keys():
+    if message.from_user.id in global_variables.inputs.keys():
         first_call = False
     else:
-        inputs[message.from_user.id] = ""
+        global_variables.inputs[message.from_user.id] = ""
 
-    inputs[message.from_user.id] += message.text
+    global_variables.inputs[message.from_user.id] += message.text
     watch_id, separator, reverse = await get_value_from_id(message.from_user.id,
                                                            fields="selectedWatch, separator, inverseSending")
     quantity_messages = len(
-        [1 for q in (await cut_into_messages(watch_id, separator, inputs[message.from_user.id])) if
+        [1 for q in (await cut_into_messages(watch_id, separator, global_variables.inputs[message.from_user.id])) if
          q != ""])
     quantity_messages_word = (lambda x: ("е" + "я" * 3 + "й" * 6)[(x - 1) % 10])(quantity_messages)
     if first_call:
@@ -59,7 +94,7 @@ async def send_cheats(message):
         debug(f"Preparing to sending to user {message.from_user.id}...")
         await asyncio.sleep(await get_value_from_id(
             message.from_user.id, fields="timeBeforeSending") - 2)  # пока задержка, досылаются сообщения
-        split_messages = await cut_into_messages(watch_id, separator, inputs[message.from_user.id])
+        split_messages = await cut_into_messages(watch_id, separator, global_variables.inputs[message.from_user.id])
         if reverse:
             split_messages = split_messages[::-1]
         while len(split_messages) > 0:
@@ -72,7 +107,7 @@ async def send_cheats(message):
             split_messages.pop(0)
             if len(split_messages) != 0:  # чтобы в конце не было задержки
                 await asyncio.sleep(seconds_between)
-        inputs.pop(message.from_user.id)
+        global_variables.inputs.pop(message.from_user.id)
         debug("Pass!")
     else:
         await message.answer(
